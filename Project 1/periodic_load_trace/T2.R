278eb096327b4d2e7bd7ce486bff5ad7fe2e27e2
Abhimanyu Babbar <abhimanyu.babbar@sics.se>
2014-11-10 18:16:16 +0100

    updating the task 2 file with latest changes

--- Project 1/periodic_load_trace/T2.R
+++ Project 1/periodic_load_trace/T2.R
@@ -1,34 +1,88 @@
-# Project 1, Task 2.
+# Project 1 Task 2
+
+
+# Read Data into X
+periodic.x = read.csv("X.csv")
+# Read Data into Y.
+periodic.y = read.csv("Y.csv")
 
 # Set the working directory.
 setwd("~/workspace/EP2400/Project 1/periodic_load_trace")
 
-# Read the Predictors.
-periodic.x = read.csv("X.csv")
 
-# Read the observed response data.
-periodic.y = read.csv("Y.csv")
+# Instead of choosing a sequential set, create a random set. (Replace = False.)
+splitdata <- function(length,seed=NULL) {
+  if(!is.null(seed)) {
+    set.seed(seed)
+  }
+  index <- 1:length
+  trainIndex <- sample(index, (0.7*length(index)) , replace=FALSE)
+}
+
+# Use any data to get a random training index. SEED = 29
+trainIndex = splitdata(nrow(periodic.x),29)
 
 # Create the training sets.
-periodic.training.x = periodic.x[1:7000,]
-periodic.training.y = periodic.y[1:7000,]
+periodic.training.x = periodic.x[trainIndex,]
+periodic.training.y = periodic.y[trainIndex,]
 
-# Create the test sets.
-periodic.test.x = periodic.x[7001:10000,]
-periodic.test.y = periodic.y[7001:10000,]
-
-
-# SubPart1 (Time Series Plot (Measurements vs Model Predictions))
-# Data Set - Test Set
-
-# Calculate the coefficients of the Linear Regression Method using the Training Set Data.
+# Create the test sets
+periodic.test.x = periodic.x[-trainIndex,]
+periodic.test.y = periodic.y[-trainIndex,]
 
 
+################## Regression Method = Least Square Regression Method.
+
+
+# Create a copy holder object.
+combinedTrainingMatrix <- periodic.training.x
+
+# Append the response data columns in the predictor data set.
+
+## Add DispFrames. 
+combinedTrainingMatrix <- cbind(combinedTrainingMatrix, DispFrames = periodic.training.y$DispFrames)
+
+## Add NoAudioPlayed
+combinedTrainingMatrix <- cbind(combinedTrainingMatrix, NoAudioPlayed = periodic.training.y$NoAudioPlayed)
+
+## Add NoRTPPkts
+combinedTrainingMatrix <- cbind(combinedTrainingMatrix, NoRTPPkts = periodic.training.y$NoRTPPkts)
+
+
+# Now perform the main linear regression tests.
+
+## Regression For DispFrames.
+lm.fit.DispFrames = lm(DispFrames~.-NoAudioPlayed, combinedTrainingMatrix)
+lm.fit.DispFrames <- update(lm.fit.DispFrames, ~.-NoRTPPkts)
+summary(lm.fit.DispFrames)
+
+## Regression For NoAudioPlayed
+lm.fit.NoAudioPlayed = lm(NoAudioPlayed~.-DispFrames, combinedTrainingMatrix)
+lm.fit.NoAudioPlayed <- update(lm.fit.NoAudioPlayed, ~.-NoRTPPkts)
+summary(lm.fit.NoAudioPlayed)
+
+
+## Regression for NoRTPPkts
+lm.fit.NoRTPPkts = lm(NoRTPPkts~.-NoAudioPlayed, combinedTrainingMatrix)
+lm.fit.NoRTPPkts <- update(lm.fit.NoRTPPkts, ~.-DispFrames)
+summary(lm.fit.NoRTPPkts)
 
 
 
-## Regression Method: (" Least Square Regression Method ")
-## Y = DispFrames
+################ Plotting Time Series Graph
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
