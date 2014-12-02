9dbd85a3c938fdd10fb2dd4319b89ed19af40c71
Ali Dar <alidar@kth.se>
2014-12-02 22:08:02 +0100

    Fixes

--- Project 2/task/Task 2/T2.R
+++ Project 2/task/Task 2/T2.R
@@ -1,15 +1,18 @@
-# Get the current Directory.
-getwd()
+args<-commandArgs(TRUE)
+x.path = "X.csv"
+y.path = "Y.csv"
+
+if (length(args) >= 2) {
+  x.path = args[1]
+  y.path = args[2]
+}
 
 # Load the predictors flash crowd data.
-flashcrowd.x <- read.csv("X.csv")
-
-# Checking components of the list.
-# flashcrowd.x[[1]] == Gives 10,000 values of the first column.
-# length(flashcrowd.x) == Gives the length of the list.
-
+stopifnot(file.exists(x.path)) # check if x data file exists
+flashcrowd.x <- read.csv(x.path)
 # Load the metrics for the flash crowd data.
-flashcrowd.y <- read.csv("Y.csv")
+stopifnot(file.exists(y.path)) # check if y data file exists
+flashcrowd.y <- read.csv(y.path)
 
 # Part 1: Classification of the metrics as "low" or "high" based on the mean of the values.
 
@@ -123,7 +126,7 @@ NoRTPPkts.glm.fit = glm(NoRTPPkts.classify ~ all_..idle + X..memused + X..swpuse
 ## Now make predictions using the test set.
 
 
-## DispFrames
+## Fit for DispFrames
 DispFrames.glm.predict <- predict(DispFrames.glm.fit, 
                               type="response",
                               newdata=flashcrowd.test.combined)
@@ -132,22 +135,24 @@ DispFrames.glm.predict <- as.data.frame(DispFrames.glm.predict)
 
 # Cleaning and converting the data
 
-# Predictor Vector.
+# Predictor Vector for DispFrames
 DispFrames.predicted.vector <- DispFrames.glm.predict$DispFrames.glm.predict
 DispFrames.predicted.vector <- replace(DispFrames.predicted.vector, DispFrames.predicted.vector >= 0.5 , "high")
-DispFrames.predicted.vector <- replace(DispFrames.predicted.vector, DispFrames.predicted.vector <  0.5 , "low")
+DispFrames.predicted.vector <- replace(DispFrames.predicted.vector, DispFrames.predicted.vector !=  'high' , "low")
+#DispFrames.predicted.vector <- replace(DispFrames.predicted.vector, DispFrames.predicted.vector <  0.5 , "low")
 
-# Actual Vector.
+# Actual Vector of DispFrames
 DispFrames.actual.vector <- flashcrowd.test.combined$DispFrames.classify
 DispFrames.actual.vector <- replace(DispFrames.actual.vector, DispFrames.actual.vector == 1 , "high")
 DispFrames.actual.vector <- replace(DispFrames.actual.vector, DispFrames.actual.vector == 0 , "low")
 
+# Creating the confusion matrix for DispFrames.
+confusion.matix.disp.frames = table(DispFrames.actual.vector,DispFrames.predicted.vector)
 
-# Creating the confusion matrix.
-table(DispFrames.predicted.vector,DispFrames.actual.vector)
+# Error rate for DispFrames
+error.rate.disp.frames = sum(confusion.matix.disp.frames[c(2, 3)])/sum(confusion.matix.disp.frames)*100 #6.8%
 
-
-## NoAudioPlayed
+## Fit for NoAudioPlayed
 NoAudioPlayed.glm.predict <- predict(NoAudioPlayed.glm.fit, 
                                   type="response",
                                   newdata=flashcrowd.test.combined)
@@ -156,21 +161,23 @@ NoAudioPlayed.glm.predict <- as.data.frame(NoAudioPlayed.glm.predict)
 
 # Cleaning and converting the data.
 
-# Predictor Vector.
+# Predictor Vector for NoAudioPlayed
 NoAudioPlayed.predicted.vector <- NoAudioPlayed.glm.predict$NoAudioPlayed.glm.predict
 NoAudioPlayed.predicted.vector <- replace(NoAudioPlayed.predicted.vector, NoAudioPlayed.predicted.vector >= 0.5 , "high")
 NoAudioPlayed.predicted.vector <- replace(NoAudioPlayed.predicted.vector, NoAudioPlayed.predicted.vector <  0.5 , "low")
 
-# Actual Vector.
+# Actual Vector for NoAudioPlayed
 NoAudioPlayed.actual.vector <- flashcrowd.test.combined$NoAudioPlayed.classify
 NoAudioPlayed.actual.vector <- replace(NoAudioPlayed.actual.vector, NoAudioPlayed.actual.vector == 1 , "high")
 NoAudioPlayed.actual.vector <- replace(NoAudioPlayed.actual.vector, NoAudioPlayed.actual.vector == 0 , "low")
 
-# Creating the confusion matrix.
-table(NoAudioPlayed.predicted.vector,NoAudioPlayed.actual.vector)
+# Creating the confusion matrix for NoAudioPlayed
+confusion.matix.no.audio.played = table(NoAudioPlayed.actual.vector,NoAudioPlayed.predicted.vector)
 
+# Error rate for NoAudioPlayed
+error.rate.no.audio.played = sum(confusion.matix.no.audio.played[c(2, 3)])/sum(confusion.matix.no.audio.played)*100 #18.1%
 
-# NoRTPPkts
+# Fit for NoRTPPkts
 NoRTPPkts.glm.predict <- predict(NoRTPPkts.glm.fit, 
                                      type="response",
                                      newdata=flashcrowd.test.combined)
@@ -179,19 +186,21 @@ NoRTPPkts.glm.predict <- as.data.frame(NoRTPPkts.glm.predict)
 
 # Cleaning and converting the data.
 
-# Predictor Vector.
+# Predictor Vector for NoRTPPkts
 NoRTPPkts.predicted.vector <- NoRTPPkts.glm.predict$NoRTPPkts.glm.predict
 NoRTPPkts.predicted.vector <- replace(NoRTPPkts.predicted.vector, NoRTPPkts.predicted.vector >= 0.5 , "high")
 NoRTPPkts.predicted.vector <- replace(NoRTPPkts.predicted.vector, NoRTPPkts.predicted.vector <  0.5 , "low")
 
-# Actual Vector.
+# Actual Vector for NoRTPPkts
 NoRTPPkts.actual.vector <- flashcrowd.test.combined$NoRTPPkts.classify
 NoRTPPkts.actual.vector <- replace(NoRTPPkts.actual.vector, NoRTPPkts.actual.vector == 1 , "high")
 NoRTPPkts.actual.vector <- replace(NoRTPPkts.actual.vector, NoRTPPkts.actual.vector == 0 , "low")
 
-# Creating the confusion matrix.
-table(NoRTPPkts.predicted.vector,NoRTPPkts.actual.vector)
+# Creating the confusion matrix for NoRTPPkts
+confusion.matix.no.rttp.pkts = table(NoRTPPkts.actual.vector, NoRTPPkts.predicted.vector)
 
+# Error rate for NoRTPPkts
+error.rate.no.rttp.pkts = sum(confusion.matix.no.rttp.pkts[c(2, 3)])/sum(confusion.matix.no.rttp.pkts)*100 # 19.4%
 
 
 ##### ==================================== END ====================================
