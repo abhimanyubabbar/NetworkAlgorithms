e9606693931a6440d2b603b9c63b60b5de74db93
Abhimanyu Babbar <abhimanyu.babbar@sics.se>
2014-12-07 12:12:49 +0100

    Initial Commit for Task 3

---
+++ Project 2/task/Task 3/T3_RandomForest.R
@@ -0,0 +1,104 @@
+
+# Reading the files.
+args<-commandArgs(TRUE)
+x.path = "X.csv"
+y.path = "Y.csv"
+
+if (length(args) >= 2) {
+  x.path = args[1]
+  y.path = args[2]
+}
+
+# Load the predictors flash crowd data.
+stopifnot(file.exists(x.path)) # check if x data file exists
+flashcrowd.x <- read.csv(x.path)
+
+# Load the metrics for the flash crowd data.
+stopifnot(file.exists(y.path)) # check if y data file exists
+flashcrowd.y <- read.csv(y.path)
+
+
+## ====================================================================================
+
+# Part 1: Classification of the metrics as "low" or "high" based on the mean of the values.
+
+# Calculating the mean of the Y Metrics.
+flashcrowd.y.mean.DispFrames <- mean(flashcrowd.y[["DispFrames"]])
+flashcrowd.y.mean.NoAudioPlayed <- mean(flashcrowd.y[["NoAudioPlayed"]])
+flashcrowd.y.mean.NoRTPPkts <- mean(flashcrowd.y[["NoRTPPkts"]])
+
+# Function to calculate the value as high or low for each of the Y metric. 
+# Replace "high" with "1" and "low" with "0".
+classify <- function(component, meanValue){
+  
+  # Create the vector to store the values.
+  x <- vector(mode="numeric", length=0)
+  
+  # Iterate over the values and perform comparison.
+  for(entry in component){
+    if(entry >= meanValue){
+      x <- c(x,1)
+    }
+    else{
+      x <- c(x,0)
+    }
+  }
+  # return the numeric vector.
+  x
+}
+
+### Start with filtering of data.
+
+# DispFrames classification vector.
+flashcrowd.y.DispFrames.classify <- classify(flashcrowd.y[["DispFrames"]],flashcrowd.y.mean.DispFrames)
+
+# NoAudioPlayed classification vector.
+flashcrowd.y.NoAudioPlayed.classify <- classify(flashcrowd.y[["NoAudioPlayed"]],flashcrowd.y.mean.NoAudioPlayed)
+
+# NoRTPPkts classification vector.
+flashcrowd.y.NoRTPPkts.classify <- classify(flashcrowd.y[["NoRTPPkts"]],flashcrowd.y.mean.NoRTPPkts)
+
+# Create a classification list.
+flashcrowd.y.classification.list <- list("DispFrames.classify"=flashcrowd.y.DispFrames.classify, "NoAudioPlayed.classify"=flashcrowd.y.NoAudioPlayed.classify, "NoRTPPkts.classify"=flashcrowd.y.NoRTPPkts.classify)
+
+
+## ============================================================
+
+## STEP 2: Divide the set into the training set and the test set.
+
+# Instead of choosing a sequential set, create a random set. (Replace = False.)
+
+splitdata <- function(length,seed=NULL) {
+  if(!is.null(seed)) {
+    set.seed(seed)
+  }
+  index <- 1:length
+  train.index.return <- sample(index, (0.7*length(index)) , replace=FALSE)
+}
+
+# Use any data to get a random training index. SEED = 29
+train.index <- splitdata(nrow(flashcrowd.combined),29)
+
+# Create the training set.
+flashcrowd.training.combined <- flashcrowd.combined[train.index,]
+
+# Create the test set.
+flashcrowd.test.combined <- flashcrowd.combined[-train.index,]
+flashcrowd.test.y = flashcrowd.y[-train.index,]
+
+
+# We now have the training and test datasets with us.
+
+# RANDOM FOREST APPROACH.
+
+
+
+
+
+
+
+
+
+
+
+
