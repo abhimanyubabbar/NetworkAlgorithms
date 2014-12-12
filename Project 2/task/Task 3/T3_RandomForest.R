048c4ce2c52f7dfccd212f8f10632a398861ba63
Abhimanyu Babbar <abhimanyu.babbar@sics.se>
2014-12-12 18:05:47 +0100

    Updating the Random Forest Calculation with the graph generation and the system execution time metrics

--- Project 2/task/Task 3/T3_RandomForest.R
+++ Project 2/task/Task 3/T3_RandomForest.R
@@ -94,13 +94,15 @@ flashcrowd.test.y = flashcrowd.y[-train.index,]
 
 # We now have the training and test datasets with us.
 
-# RANDOM FOREST APPROACH.
+# RANDOM FOREST APPROACH =================================================.
 
 # Install Package if not present.
 # install.packages("randomForest")
 
 library(randomForest)
 set.seed(1)
+
+
 
 # Set the class of the y metrics as factors.
 flashcrowd.training.combined$DispFrames.classify <- factor(flashcrowd.training.combined$DispFrames.classify)
@@ -121,20 +123,46 @@ DispFrames.rf <- randomForest(
 
 
 # Now predict the values.
-DispFrames.prediction = predict(DispFrames.rf,
+DispFrames.predicted.vector = predict(DispFrames.rf,
                                 newdata = flashcrowd.test.combined)
 
+DispFrames.actual.vector <- flashcrowd.test.combined$DispFrames.classify
 
 # Confusion Matrix + Error Rate.
-confusion.matix.disp.frames <- table(flashcrowd.test.combined$DispFrames.classify,DispFrames.prediction)
+confusion.matix.disp.frames <- table( DispFrames.actual.vector, DispFrames.predicted.vector)
 error.rate.disp.frames = sum(confusion.matix.disp.frames[c(2, 3)])/sum(confusion.matix.disp.frames)*100 
 
 # Comparison
 # Original <- 6.8 %
 # Random Forest <- 5.33 %
 
+# Time Series Plot.
 
-# ======================== 
+pch.values.dispframes = ifelse(DispFrames.predicted.vector == DispFrames.actual.vector, 1, 0)
+col.values.dispframes = ifelse(DispFrames.predicted.vector == DispFrames.actual.vector, "blue", "red")
+
+png(file="timeseries_classification_rf_dispframes.png",width=500, height=400)
+plot(DispFrames~c(1:nrow(flashcrowd.test.y)), 
+     data=flashcrowd.test.y, 
+     pch = pch.values.dispframes, col=col.values.dispframes,
+     ylab="Video Frame Rate", xlab="Times",
+     main="Video Frame Rate Random Forest Classification")
+abline(h=flashcrowd.y.mean.DispFrames, col="green")
+legend("topleft", 
+       legend = c("Correctly Classfied", "Incorrectly Classified"), 
+       pch=c(1, 0),
+       col=c("blue", "red"))
+dev.off()
+
+#Execution time for fitting DispFrames
+exec.time.fit.dispframes = system.time(randomForest(
+  as.factor(DispFrames.classify) ~ .,
+  data = subset(flashcrowd.training.combined, select = -c(NoAudioPlayed.classify,NoRTPPkts.classify)),
+  mtry = 3,     # The mtry component in case of classification is best for root(predictor_size)
+  importance = TRUE
+)) # 0.070 sec
+
+# ========================
 
 # NoAudioPlayed
 NoAudioPlayed.rf <- randomForest(
@@ -146,11 +174,13 @@ NoAudioPlayed.rf <- randomForest(
 
 
 # Now predict the values.
-NoAudioPlayed.prediction = predict(NoAudioPlayed.rf,
+NoAudioPlayed.predicted.vector <- predict(NoAudioPlayed.rf,
                                 newdata = flashcrowd.test.combined)
 
+NoAudioPlayed.actual.vector <- flashcrowd.test.combined$NoAudioPlayed.classify
+
 # Confusion Matrix + Error Rate.
-confusion.matix.no.audio.played <- table(flashcrowd.test.combined$NoAudioPlayed.classify,NoAudioPlayed.prediction)
+confusion.matix.no.audio.played <- table(NoAudioPlayed.actual.vector, NoAudioPlayed.predicted.vector)
 error.rate.no.audio.played = sum(confusion.matix.no.audio.played[c(2, 3)])/sum(confusion.matix.no.audio.played)*100 #18.1%
 
 
@@ -158,6 +188,31 @@ error.rate.no.audio.played = sum(confusion.matix.no.audio.played[c(2, 3)])/sum(c
 # Original <- 18.1 %
 # Random Forest <- 1.06 %
 
+# Time series plot that shows the measurements for samples in the test set for NoAudioPlayed
+
+pch.values.no.audio.played = ifelse(NoAudioPlayed.predicted.vector == NoAudioPlayed.actual.vector, 1, 0)
+col.values.no.audio.played = ifelse(NoAudioPlayed.predicted.vector == NoAudioPlayed.actual.vector, "blue", "red")
+
+png(file="timeseries_classification_rf_noaudioplayed.png",width=500, height=400)
+plot(NoAudioPlayed~c(1:nrow(flashcrowd.test.y)), 
+     data=flashcrowd.test.y, 
+     pch = pch.values.no.audio.played, col=col.values.no.audio.played,
+     ylab="Audio Buffer Rate", xlab="Times",
+     main="Audio Buffer Rate Random Forest Classification")
+abline(h=flashcrowd.y.mean.NoAudioPlayed, col="green")
+legend("topleft", 
+       legend = c("Correctly Classfied", "Incorrectly Classified"), 
+       pch=c(1, 0),
+       col=c("blue", "red"))
+dev.off()
+
+#Execution time for fitting NoAudioPlayed
+exec.time.fit.no.audio.played = system.time(randomForest(
+  as.factor(NoAudioPlayed.classify) ~ .,
+  data = subset(flashcrowd.training.combined, select = -c(DispFrames.classify,NoRTPPkts.classify)),
+  mtry = 3,     # The mtry component in case of classification is best for root(predictor_size)
+  importance = TRUE
+)) # 0.099 sec
 
 # =============================
 
@@ -172,17 +227,46 @@ NoRTPPkts.rf <- randomForest(
 
 
 # Now predict the values.
-NoRTPPkts.prediction = predict(NoRTPPkts.rf,
+NoRTPPkts.predicted.vector <- predict(NoRTPPkts.rf,
                                    newdata = flashcrowd.test.combined)
 
+NoRTPPkts.actual.vector <- flashcrowd.test.combined$NoRTPPkts.classify
+
 # Confusion Matrix + Error Rate.
-confusion.matix.no.rttp.pkts <- table(flashcrowd.test.combined$NoRTPPkts.classify,NoRTPPkts.prediction)
+confusion.matix.no.rttp.pkts <- table(NoRTPPkts.actual.vector, NoRTPPkts.predicted.vector)
 error.rate.no.rttp.pkts = sum(confusion.matix.no.rttp.pkts[c(2, 3)])/sum(confusion.matix.no.rttp.pkts)*100 
 
 
 # Comparison
 # Original <- 19.4 %
 # Random Forest <- 7.73 %
+
+
+# Time series plot that shows the measurements for samples in the test set for NoRTPPkts
+pch.values.no.rttp.pkts = ifelse(NoRTPPkts.predicted.vector == NoRTPPkts.actual.vector, 1, 0)
+col.values.no.rttp.pkts = ifelse(NoRTPPkts.predicted.vector == NoRTPPkts.actual.vector, "blue", "red")
+
+png(file="timeseries_classification_rf_nortppacket.png",width=500, height=400)
+plot(NoRTPPkts~c(1:nrow(flashcrowd.test.y)), 
+     data=flashcrowd.test.y, 
+     pch = pch.values.no.rttp.pkts, col=col.values.no.rttp.pkts,
+     ylab="RTP Packet Rate", xlab="Times",
+     main="RTP Packet Rate Random Forest Classification")
+abline(h=flashcrowd.y.mean.NoRTPPkts, col="green")
+legend("topleft", 
+       legend = c("Correctly Classfied", "Incorrectly Classified"), 
+       pch=c(1, 0),
+       col=c("blue", "red"))
+dev.off()
+
+#Execution time for fitting NoAudioPlayed
+exec.time.fit.no.rttp.pkts = system.time(randomForest(
+  as.factor(NoRTPPkts.classify) ~ .,
+  data = subset(flashcrowd.training.combined, select = -c(DispFrames.classify,NoAudioPlayed.classify)),
+  mtry = 3,     # The mtry component in case of classification is best for root(predictor_size)
+  importance = TRUE
+)) # 0.074 sec
+
 
 
 # =================================================
